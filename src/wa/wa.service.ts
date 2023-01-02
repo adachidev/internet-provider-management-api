346c1f1ce2ec12644d97b9af98c598672f9eaa9f
Rodrigo Adachi <adachi.rodrigo@gmail.com>
2023-01-02 09:50:23 -0300

    phone wa

--- src/wa/wa.service.ts
+++ src/wa/wa.service.ts
@@ -18,7 +18,24 @@ export class WaService {
   constructor() {}
 
   async sendText(phone: string, message: string, token: string) {
-    const phoneNumber = phone.replace(/\D/gim, '');
+    let phoneNumber = phone.replace(/\D/gim, '');
+
+    if (phoneNumber.length < 8 || phoneNumber.length > 13) return 'PHONE_ERROR';
+
+    phoneNumber =
+      phoneNumber.length === 13
+        ? phoneNumber // 5571 92590635
+        : phoneNumber.length === 12
+        ? phoneNumber.slice(0, 4) + 9 + phoneNumber.slice(4, 12)
+        : phoneNumber.length === 11
+        ? 55 + phoneNumber
+        : phoneNumber.length === 10
+        ? 55 + phoneNumber.slice(0, 2) + 9 + phoneNumber.slice(2, 10)
+        : phoneNumber.length === 9
+        ? 5571 + phoneNumber
+        : phoneNumber.length === 8
+        ? 55719 + phoneNumber
+        : phoneNumber;
 
     if (token != process.env.WA_PASS) return 'TOKEN_ERROR';
 
